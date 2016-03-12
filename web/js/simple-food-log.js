b415ce6ee6b789bbaeefad13bf2da5f099daf9ef
Vasja Volin <vasja.volin@gmail.com>
2016-03-12 17:33:05 -0500

    add barely working food list

--- web/js/simple-food-log.js
+++ web/js/simple-food-log.js
@@ -1,8 +1,35 @@
 var app = {};
-firstFood = { index: 0, food: 'Bacon 120 cal', calories: '120' };
+var firstFood = { index: 0, entry: 'Bacon 120 cal', calories: '120' };
+var secondFood = { index: 1, entry: 'Eggs 170 cal', calories: '170' };
+var thirdFood = { index: 2, entry: 'Orange Juice 100 cal', calories: '100' };
+var foods = [firstFood, secondFood, thirdFood];
 
-var FoodEntry = React.createElement('li', {}, firstFood.food);
-var root = React.createElement('ul', {}, FoodEntry)
+// var FoodEntry = React.createElement('li', {}, this.props.food);
+var FoodEntry = React.createClass({
+    propTypes: {
+        entry: React.PropTypes.string.isRequired,
+    },
+
+    render: function() {
+        return (
+            React.createElement('li', { key: this.props.index }, this.props.entry)
+        );
+    }
+});
+
+var listFoods = foods.map(function(food) {
+    return React.createElement(FoodEntry, food);
+});
+
+var FoodList = React.createClass({
+    render: function() {
+        return (
+            React.createElement('ul', {}, this.props.foods)
+        );
+    }
+});
+
+var root = React.createElement(FoodList, { foods: listFoods });
 ReactDOM.render(root, document.getElementById('food-list'));
 
 /* tests below */
