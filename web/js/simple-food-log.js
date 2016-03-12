94c2291f78770022301755ea8a5b377b5573a1ea
Vasja Volin <vasja.volin@gmail.com>
2016-03-12 17:52:46 -0500

    add function for calculating food calorie total from a list of foods

--- web/js/simple-food-log.js
+++ web/js/simple-food-log.js
@@ -4,6 +4,12 @@ var secondFood = { key: 1, entry: 'Eggs 170 cal', calories: '170' };
 var thirdFood = { key: 2, entry: 'Orange Juice 100 cal', calories: '100' };
 var foods = [firstFood, secondFood, thirdFood];
 
+var foodTotal = function(foods) {
+    return foods.reduce(function(previous, current) {
+        return previous + parseInt(current.calories);
+    }, 0);
+};
+
 var FoodEntry = React.createClass({
     propTypes: {
         entry: React.PropTypes.string.isRequired,
@@ -31,4 +37,5 @@ var root = React.createElement(FoodList, { foods: foods });
 ReactDOM.render(root, document.getElementById('food-list'));
 
 /* tests below */
-// console.assert(2 < 2, 'failed asserting 1 less than 2');
+fakeFoods = [{ calories: 100 }, { calories: 250 }, { calories: 150 }];
+console.assert(foodTotal(fakeFoods) == 500 , 'failed calculating total calories');
