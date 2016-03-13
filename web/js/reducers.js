9b73f2d92a8be3b63e2d175cbe966cfeb0a6d61a
Vasja Volin <vasja.volin@gmail.com>
2016-03-12 23:57:35 -0500

    add a reducers file and a sample reducer

---
+++ web/js/reducers.js
@@ -0,0 +1,10 @@
+// I'm going to start with a single, all-encompassing reducer. Once I
+// understand more, I will convert to multiple reducers, and combine
+// them using Dan Abramov's method
+
+var sflReducer = function(state, action) {
+    return state;
+};
+
+// tests
+
