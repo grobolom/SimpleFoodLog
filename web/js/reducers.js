1baf44c93a836d34b952b7f76caa438b7e4e98ef
Vasja Volin <vasja.volin@gmail.com>
2016-03-13 00:29:24 -0500

    add a small, tested reducer that adds todos

--- web/js/reducers.js
+++ web/js/reducers.js
@@ -2,9 +2,23 @@
 // understand more, I will convert to multiple reducers, and combine
 // them using Dan Abramov's method
 
-var sflReducer = function(state, action) {
+var sflReducer = function(state = {}, action) {
+    if (action.type === 'ADD_FOOD') {
+        return Object.assign({}, state, {
+            foods: state.foods.concat(action.entry)
+        });
+    }
     return state;
 };
 
 // tests
 
+var testInitialState = { foods: [] };
+var testAddTodoAction = {
+    type: 'ADD_FOOD',
+    entry: { index: '1', entry: 'Bacon Bits 200' }
+};
+var expectedNextState = { foods: [testAddTodoAction.entry] };
+var nextState = sflReducer(testInitialState, testAddTodoAction);
+
+console.assert(_.isEqual(expectedNextState, nextState), 'adds food');
